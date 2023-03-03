5df8d47e8e4d2a7bb916a697473cc13c4cad7ffe
Diego Beltran <diego.beltran88@gmail.com>
2023-03-03 15:29:02 -0300

    Validaciones de grupo

--- src/beltranus.ts
+++ src/beltranus.ts
@@ -8,6 +8,10 @@ const prefixWenchotino = 'wenchotino';
 const prefixBel = 'bel';
 const prefixRoboto = 'roboto';
 
+const gruposBeltranus = ['Familia B&G', 'Hermanitos'];
+const gruposWenchotino = ['Corvo 👺'];
+const gruposRoboto = ['Test 5'];
+
 export class Beltranus {
 
   private prefix = 'bel';
@@ -20,42 +24,42 @@ export class Beltranus {
     this.chatGpt = new ChatGTP();
   }
 
+
+  private async getPrompt(message: Message, chatData: Chat): Promise<PromptName | null> {
+    const tieneWenchotino = tienePrefix(message.body, prefixWenchotino);
+    const tieneBel = tienePrefix(message.body, prefixBel);
+    const tieneRoboto = tienePrefix(message.body, prefixRoboto);
+
+    const meResponden = message.hasQuotedMsg ? (await message.getQuotedMessage()).fromMe : false;
+
+    if(tieneBel || (meResponden && gruposBeltranus.includes(chatData.name)))
+      return PromptName.BELTRANUS;
+    else if(tieneWenchotino || (meResponden && gruposWenchotino.includes(chatData.name)))
+      return PromptName.WENCHOTINO;
+    else if(tieneRoboto || (meResponden && gruposRoboto.includes(chatData.name)) || !chatData.isGroup)
+      return PromptName.ROBOTO;
+    else
+      return null;
+  }
+
   public async readMessage(message: Message) {
     try {
-      let   esWenchotino = tienePrefix(message.body, prefixWenchotino);
-      let   esBel = tienePrefix(message.body, prefixBel);
-      let   esRoboto = tienePrefix(message.body, prefixRoboto);
-
-      const tieneCommand = message.body.substring(0, 3) == this.commandPrefix+'a ';
-      let   prompt: PromptName = this.getPrompt(message.body);
-
+      /** Se reciben datos de entrada */
       const chatData: Chat = await message.getChat();
-      const quotedMessage = await message.getQuotedMessage();
+      const tieneCommand = message.body.substring(0, 3) == this.commandPrefix+'a ';
 
-
-      if(quotedMessage?.fromMe && (chatData.name == GrupoName.FAMILIA || chatData.name == GrupoName.TEST)){
-        esBel = true;
-        prompt = PromptName.BELTRANUS;
-      }
-
-      if(!esWenchotino && !esBel && !esRoboto) return;
-
-      let messageContent = '';
-      let contactInfo;
-
-      if (tieneCommand && !esBel) {
-        const {command, content} = getMsgData(message);
-        contactInfo = await message.getContact();
-        return this.commandSelect(message, command, contactInfo.name || 'Alguien');
-      }
-
-      messageContent = message.body;
+      /** Se evalua si corresponde a algun bot */
+      let   prompt: PromptName | null = await this.getPrompt(message, chatData);
+      if(prompt == null && !tieneCommand) return false;
 
       logMessage(message, chatData);
 
-      contactInfo = await message.getContact();
+      /** Datos de contacto */
+      const contactInfo = await message.getContact();
+
+      /** Envia mensaje a ChatGPT */
       chatData.sendStateTyping();
-      await this.chatGPTReply(message, messageContent, contactInfo.name || 'Alguien', prompt);
+      await this.chatGPTReply(message, message.body, contactInfo.name || 'Alguien', prompt as PromptName);
       chatData.clearState();
       return true;
     } catch (e) {
@@ -79,15 +83,6 @@ export class Beltranus {
     return await message.reply(responseChat);
   }
 
-  private getPrompt(msg: string): PromptName{
-    const esWenchotino = tienePrefix(msg, prefixWenchotino);
-    const esBel = tienePrefix(msg, prefixBel);
-    const esRoboto = tienePrefix(msg, prefixRoboto);
-
-    if(esWenchotino) return PromptName.WENCHOTINO;
-    else if(esBel) return PromptName.BELTRANUS;
-    else return PromptName.ROBOTO;
-  }
 
   private async commandSelect(message: Message, command: string, contactName: string) {
     switch (command) {
@@ -101,6 +96,50 @@ export class Beltranus {
   private async customMp3(message) {
     return await message.reply('mp3');
   }
+
+  // public async readMessage(message: Message) {
+  //   try {
+  //     let   esWenchotino = tienePrefix(message.body, prefixWenchotino);
+  //     let   esBel = tienePrefix(message.body, prefixBel);
+  //     let   esRoboto = tienePrefix(message.body, prefixRoboto);
+  //
+  //     const tieneCommand = message.body.substring(0, 3) == this.commandPrefix+'a ';
+  //     let   prompt: PromptName = this.getPrompt(message.body);
+  //
+  //     const chatData: Chat = await message.getChat();
+  //     const quotedMessage = await message.getQuotedMessage();
+  //
+  //
+  //     // if(quotedMessage?.fromMe && (chatData.name == GrupoName.FAMILIA || chatData.name == GrupoName.TEST)){
+  //     if(quotedMessage?.fromMe && (chatData.name == GrupoName.FAMILIA || chatData.name == GrupoName.TEST)){
+  //       esBel = true;
+  //       prompt = PromptName.BELTRANUS;
+  //     }
+  //
+  //     if(!esWenchotino && !esBel && !esRoboto) return;
+  //
+  //     let messageContent = '';
+  //     let contactInfo;
+  //
+  //     if (tieneCommand && !esBel) {
+  //       const {command, content} = getMsgData(message);
+  //       contactInfo = await message.getContact();
+  //       return this.commandSelect(message, command, contactInfo.name || 'Alguien');
+  //     }
+  //
+  //     messageContent = message.body;
+  //
+  //     logMessage(message, chatData);
+  //
+  //     contactInfo = await message.getContact();
+  //     chatData.sendStateTyping();
+  //     await this.chatGPTReply(message, messageContent, contactInfo.name || 'Alguien', prompt);
+  //     chatData.clearState();
+  //     return true;
+  //   } catch (e) {
+  //     handleError(e, message);
+  //   }
+  // }
 
 
 }
