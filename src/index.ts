4a8f0f1259caa53f1d28e427bba59ddd4bd7bff3
diego-beltran-globalpay <diego.beltrangalvez@globalpay.com>
2024-10-18 16:58:52 -0300

    Fix Index 1

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,11 @@ import {Beltranus} from './beltranus';
 const qrcode = require('qrcode-terminal');
 const { Client } = require('whatsapp-web.js');
 const client = new Client({
-  webVersionCache:
-    {
-      path: './local-html/2.2413.51-beta-alt.html',
-      type: 'local'
-    }
+  webVersionCache: {
+    type: "remote",
+    remotePath:
+      "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
+  },
 });
 
 
